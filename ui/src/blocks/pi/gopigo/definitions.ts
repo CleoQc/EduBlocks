9cdb009e73eb95d2609c6a87c6da7e5af6bc3269
CleoQc <cleoqc1124@gmail.com>
2020-06-13 15:50:46 -0400

    GoPiGo and LED blocks

--- ui/src/blocks/pi/gopigo/definitions.ts
+++ ui/src/blocks/pi/gopigo/definitions.ts
@@ -3,6 +3,7 @@ export default function define(Blocks: Blockly.BlockDefinitions) {
   var maincolour = "#CC3333";
   var bordercolour = "#999999";
   var inputcolour = "#CC3333";
+
   Blocks['import_easygopigo3'] = {
     init: function () {
       this.appendDummyInput()
@@ -10,7 +11,7 @@ export default function define(Blocks: Blockly.BlockDefinitions) {
       this.setPreviousStatement(true, null);
       this.setNextStatement(true, null);
       this.setColour(maincolour, inputcolour, bordercolour);
-      this.setTooltip('Imports the easygopigo3 library.');
+      // this.setTooltip('Imports the easygopigo3 library.');
     },
   };
   Blocks['gpgset'] = {
@@ -25,8 +26,8 @@ export default function define(Blocks: Blockly.BlockDefinitions) {
       this.setPreviousStatement(true, null);
       this.setNextStatement(true, null);
       this.setColour(maincolour, inputcolour, bordercolour);
-      this.setTooltip('Set a variable for the GopiGo3');
-      this.setHelpUrl('https://gopigo3.readthedocs.io/en/master/api-basic/easygopigo3.html#easygopigo3.EasyGoPiGo3');
+      // this.setTooltip('Set a variable for the GopiGo3');
+      // this.setHelpUrl('https://gopigo3.readthedocs.io/en/master/api-basic/easygopigo3.html#easygopigo3.EasyGoPiGo3');
     },
   };
 
@@ -42,10 +43,128 @@ export default function define(Blocks: Blockly.BlockDefinitions) {
       this.setPreviousStatement(true, null);
       this.setNextStatement(true, null);
       this.setColour(maincolour, inputcolour, bordercolour);
-      this.setTooltip('Set speed');
-      this.setHelpUrl('https://gopigo3.readthedocs.io/en/master/api-basic/easygopigo3.html#easygopigo3.EasyGoPiGo3.set_speed');
+      // this.setTooltip('Set speed');
+      // this.setHelpUrl('https://gopigo3.readthedocs.io/en/master/api-basic/easygopigo3.html#easygopigo3.EasyGoPiGo3.set_speed');
     },
   };
+
+  Blocks['gpggetvolt'] = {
+    init: function () {
+      this.appendDummyInput()
+        .appendField(new Blockly.FieldVariable('voltage'), 'voltage')
+        .appendField(' = ')
+        .appendField(new Blockly.FieldVariable('gpg'), 'gpg')
+        .appendField('.volt()')
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(maincolour, inputcolour, bordercolour);
+      this.setTooltip('Get GopiGo3 speed');
+      // this.setHelpUrl('https://gopigo3.readthedocs.io/en/master/api-basic/easygopigo3.html#easygopigo3.EasyGoPiGo3');
+    },
+  };
+
+  Blocks['gpgstop'] = {
+    init: function () {
+      this.appendDummyInput()
+        .appendField(new Blockly.FieldVariable('gpg'), 'gpg')
+        .appendField('.stop()')
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(maincolour, inputcolour, bordercolour);
+      this.setTooltip('Stop GopiGo3');
+      // this.setHelpUrl('https://gopigo3.readthedocs.io/en/master/api-basic/easygopigo3.html#easygopigo3.EasyGoPiGo3');
+    },
+  };
+
+  Blocks['gpggetspeed'] = {
+    init: function () {
+      this.appendDummyInput()
+        .appendField(new Blockly.FieldVariable('speed'), 'speed')
+        .appendField(' = ')
+        .appendField(new Blockly.FieldVariable('gpg'), 'gpg')
+        .appendField('.get_speed()')
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(maincolour, inputcolour, bordercolour);
+      this.setTooltip('Get GopiGo3 speed');
+      // this.setHelpUrl('https://gopigo3.readthedocs.io/en/master/api-basic/easygopigo3.html#easygopigo3.EasyGoPiGo3');
+    },
+  };
+
+  Blocks['gpgmovement'] = {
+    init: function () {
+      this.appendDummyInput()
+        .appendField(new Blockly.FieldVariable('gpg'), 'gpg')
+        .appendField('.')
+        .appendField(new Blockly.FieldDropdown([
+          ['drive_cm', 'drive_cm'], 
+          ['drive_inches', 'drive_inches'],
+          ['drive_degrees', 'drive_degrees'],
+          ['turn_degrees', 'turn_degrees'],
+        ]), 'action')
+        .appendField('(')
+      this.appendValueInput("value")
+        .setCheck(null)
+      this.appendDummyInput()
+        .appendField(', blocking=')
+        .appendField(new Blockly.FieldDropdown([
+          ['True', 'True'], 
+          ['False', 'False'],
+        ]), 'blocking')
+      this.appendDummyInput()
+        .appendField(')');
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(maincolour, inputcolour, bordercolour);
+      this.setTooltip('Set gopigo movement');
+      // this.setHelpUrl('https://gpiozero.readthedocs.io/en/stable/api_input.html#light-sensor-ldr');
+    },
+  };
+
+  Blocks['gpgsteer'] = {
+    init: function () {
+      this.appendDummyInput()
+        .appendField(new Blockly.FieldVariable('gpg'), 'gpg')
+        .appendField('.steer(left_percent=')
+      this.appendValueInput("value1")
+        .setCheck(null)
+        this.appendDummyInput()
+          .appendField(', right_percent=')
+        this.appendValueInput("value2")
+        .setCheck(null)
+      this.appendDummyInput()
+        .appendField(')');
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(maincolour, inputcolour, bordercolour);
+      this.setTooltip('Set gopigo movement');
+      // this.setHelpUrl('https://gpiozero.readthedocs.io/en/stable/api_input.html#light-sensor-ldr');
+    },
+  };
+
+  Blocks['gpgorbit'] = {
+    init: function () {
+      this.appendDummyInput()
+        .appendField(new Blockly.FieldVariable('gpg'), 'gpg')
+        .appendField('.orbit(degrees=')
+      this.appendValueInput("value1")
+        .setCheck(null)
+        this.appendDummyInput()
+          .appendField(', radius_cm=')
+        this.appendValueInput("value2")
+        .setCheck(null)
+      this.appendDummyInput()
+        .appendField(')');
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(maincolour, inputcolour, bordercolour);
+      // this.setTooltip('Set gopigo movement');
+      // this.setHelpUrl('https://gpiozero.readthedocs.io/en/stable/api_input.html#light-sensor-ldr');
+    },
+  };
+
+
+
   Blocks['gpgmovement_noarg'] = {
     init: function () {
       this.appendDummyInput()
@@ -58,14 +177,89 @@ export default function define(Blocks: Blockly.BlockDefinitions) {
           ['left()', 'left'],
           ['spin_right()', 'spin_right'],
           ['spin_left()', 'spin_left'],
-          ['stop()', 'stop'],
         ]), 'action');
       this.setPreviousStatement(true, null);
       this.setNextStatement(true, null);
       this.setColour(maincolour, inputcolour, bordercolour);
-      this.setTooltip('Set gopigo movement');
+      // this.setTooltip('Set gopigo movement');
       // this.setHelpUrl('https://gpiozero.readthedocs.io/en/stable/api_input.html#light-sensor-ldr');
     },
   };
+
+  /////////////////////////////////////////////////////////////////////////
+  //
+  // LIGHTS
+  //
+  /////////////////////////////////////////////////////////////////////////
+
+  Blocks['gpgseteyecolor'] = {
+    init: function () {
+      this.appendDummyInput()
+        .appendField(new Blockly.FieldVariable('gpg'), 'gpg')
+        .appendField('.')
+        .appendField(new Blockly.FieldDropdown([
+          ['set_eye_color', 'set_eye_color'], 
+          ['set_left_eye_color', 'set_left_eye_color'],
+          ['set_right_eye_color', 'set_right_eye_color'],
+        ]), 'action')
+        .appendField("(color=");
+      this.appendValueInput("color")
+        .setCheck(null)
+        this.appendDummyInput()
+        .appendField(")");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(maincolour, inputcolour, bordercolour);
+      // this.setTooltip('Set gopigo movement');
+      // this.setHelpUrl('https://gpiozero.readthedocs.io/en/stable/api_input.html#light-sensor-ldr');
+    },
+  };
+
+  Blocks['gpgopencloseyes'] = {
+    init: function () {
+      this.appendDummyInput()
+        .appendField(new Blockly.FieldVariable('gpg'), 'gpg')
+        .appendField('.')
+        .appendField(new Blockly.FieldDropdown([
+          ['open_eyes()', 'open_eyes'], 
+          ['open_left_eye()', 'open_left_eye'],
+          ['open_right_eye()', 'open_right_eye'], 
+          ['close_eyes()', 'close_eyes'],
+          ['close_left_eye()', 'close_left_eye'], 
+          ['close_right_eye()', 'close_right_eye'],
+        ]), 'action')
+        ;
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(maincolour, inputcolour, bordercolour);
+      // this.setTooltip('Set gopigo movement');
+      // this.setHelpUrl('https://gpiozero.readthedocs.io/en/stable/api_input.html#light-sensor-ldr');
+    },
+  };
+
+  Blocks['gpgblinkers'] = {
+    init: function () {
+      this.appendDummyInput()
+        .appendField(new Blockly.FieldVariable('gpg'), 'gpg')
+        .appendField('.')
+        .appendField(new Blockly.FieldDropdown([
+          ['blinker_on', 'blinker_on'], 
+          ['blinker_off', 'blinker_off'],
+        ]), 'action')
+        .appendField("('")
+        .appendField(new Blockly.FieldDropdown([
+          ['left', 'left'], 
+          ['right', 'right'],
+        ]), 'which')
+        .appendField("')")
+        ;
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(maincolour, inputcolour, bordercolour);
+      // this.setTooltip('Set gopigo movement');
+      // this.setHelpUrl('https://gpiozero.readthedocs.io/en/stable/api_input.html#light-sensor-ldr');
+    },
+  };
+
 }
 
--- ui/src/blocks/pi/gopigo/generators.ts
+++ ui/src/blocks/pi/gopigo/generators.ts
@@ -3,17 +3,67 @@ export default function define(Python: Blockly.BlockGenerators) {
     const code = 'from easygopigo3 import EasyGoPiGo3\n';
     return code;
   };
+
   Python['gpgset'] = function (block) {
     const variable_name = Blockly.Python.variableDB_.getName(block.getFieldValue('gpg'), Blockly.Variables.NAME_TYPE);
     // var text_pin = Blockly.Python.valueToCode(block, 'text', Blockly.Python.ORDER_ATOMIC);
     const code = variable_name + ' = EasyGoPiGo3()\n';
     return code;
   };
+
   Python['gpgsetspeed'] = function (block) {
     const variable_name = Blockly.Python.variableDB_.getName(block.getFieldValue('gpg'), Blockly.Variables.NAME_TYPE);
     var text_bracket = Blockly.Python.valueToCode(block, 'speed', Blockly.Python.ORDER_ATOMIC);
-    // TODO: Assemble Python into code variable.
+    // Assemble Python into code variable.
     const code = variable_name + '.set_speed(in_speed=' + text_bracket + ')\n';
+    return code;
+  };
+
+  Python['gpggetspeed'] = function (block) {
+    const variable_name = Blockly.Python.variableDB_.getName(block.getFieldValue('speed'), Blockly.Variables.NAME_TYPE);
+    const robot_name = Blockly.Python.variableDB_.getName(block.getFieldValue('gpg'), Blockly.Variables.NAME_TYPE);
+    const code = variable_name + '= ' + robot_name + '.get_speed()\n';
+    return code;
+  }; 
+
+  Python['gpggetvolt'] = function (block) {
+    const variable_name = Blockly.Python.variableDB_.getName(block.getFieldValue('voltage'), Blockly.Variables.NAME_TYPE);
+    const robot_name = Blockly.Python.variableDB_.getName(block.getFieldValue('gpg'), Blockly.Variables.NAME_TYPE);
+    const code = variable_name + '= ' + robot_name + '.volt()\n';
+    return code;
+  }; 
+
+  Python['gpgstop'] = function (block) {
+    const robot_name = Blockly.Python.variableDB_.getName(block.getFieldValue('gpg'), Blockly.Variables.NAME_TYPE);
+    const code = robot_name + '.stop()\n';
+    return code;
+  }; 
+
+  Python['gpgmovement'] = function (block) {
+    const variable_name = Blockly.Python.variableDB_.getName(block.getFieldValue('gpg'), Blockly.Variables.NAME_TYPE);
+    const dropdown_action = block.getFieldValue('action');    
+    const dropdown_blocking = block.getFieldValue('blocking');
+    var dist_bracket = Blockly.Python.valueToCode(block, 'value', Blockly.Python.ORDER_ATOMIC);
+    // Assemble Python into code variable.
+    const code = variable_name + '.' + dropdown_action + '('+ dist_bracket + ', blocking='+ dropdown_blocking+ ')\n';
+    return code;
+  };
+
+  Python['gpgsteer'] = function (block) {
+    const variable_name = Blockly.Python.variableDB_.getName(block.getFieldValue('gpg'), Blockly.Variables.NAME_TYPE);
+    var text_bracket1 = Blockly.Python.valueToCode(block, 'value1', Blockly.Python.ORDER_ATOMIC);
+    var text_bracket2 = Blockly.Python.valueToCode(block, 'value2', Blockly.Python.ORDER_ATOMIC);
+    // Assemble Python into code variable.
+    const code = variable_name + '.steer(left_percent=' + text_bracket1 + ', right_percent='+ text_bracket2 + ')\n';
+    return code;
+  };
+
+  Python['gpgorbit'] = function (block) {
+    const variable_name = Blockly.Python.variableDB_.getName(block.getFieldValue('gpg'), Blockly.Variables.NAME_TYPE);
+    var text_bracket1 = Blockly.Python.valueToCode(block, 'value1', Blockly.Python.ORDER_ATOMIC);
+    var text_bracket2 = Blockly.Python.valueToCode(block, 'value2', Blockly.Python.ORDER_ATOMIC);
+    // Assemble Python into code variable.
+    const code = variable_name + '.orbit(degrees=' + text_bracket1 + ', radius_cm='+ text_bracket2 + ')\n';
     return code;
   };
 
@@ -21,8 +71,41 @@ export default function define(Python: Blockly.BlockGenerators) {
     const variable_name = Blockly.Python.variableDB_.getName(block.getFieldValue('gpg'), Blockly.Variables.NAME_TYPE);
     const dropdown_action = block.getFieldValue('action');
     // var text_bracket = Blockly.Python.valueToCode(block, 'text', Blockly.Python.ORDER_ATOMIC);
-    // TODO: Assemble Python into code variable.
+    // Assemble Python into code variable.
     const code = variable_name + '.' + dropdown_action + '()\n';
+    return code;
+  };
+
+  /////////////////////////////////////////////////////////////////////////
+  //
+  // LIGHTS
+  //
+  /////////////////////////////////////////////////////////////////////////
+
+  Python['gpgseteyecolor'] = function (block) {
+    const variable_name = Blockly.Python.variableDB_.getName(block.getFieldValue('gpg'), Blockly.Variables.NAME_TYPE);
+    var text_bracket = Blockly.Python.valueToCode(block, 'color', Blockly.Python.ORDER_ATOMIC);
+    // Assemble Python into code variable.
+    const code = variable_name + '.seteyecolor('+ text_bracket + "')\n"
+    return code;
+  };
+
+  Python['gpgopencloseyes'] = function (block) {
+    const variable_name = Blockly.Python.variableDB_.getName(block.getFieldValue('gpg'), Blockly.Variables.NAME_TYPE);
+    const dropdown_action = block.getFieldValue('action');
+    // Assemble Python into code variable.
+    const code = variable_name + "."+dropdown_action+ "\n";
+    return code;
+  };
+
+
+  Python['gpgblinkers'] = function (block) {
+    const variable_name = Blockly.Python.variableDB_.getName(block.getFieldValue('gpg'), Blockly.Variables.NAME_TYPE);
+    const dropdown_action = block.getFieldValue('action');
+    const dropdown_which = block.getFieldValue('which');
+    // var text_bracket = Blockly.Python.valueToCode(block, 'text', Blockly.Python.ORDER_ATOMIC);
+    // Assemble Python into code variable.
+    const code = variable_name + '.' + dropdown_action + "('"+ dropdown_which +"')\n"
     return code;
   };
 }
