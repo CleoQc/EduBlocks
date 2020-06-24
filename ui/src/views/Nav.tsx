67a5e3c2ce5f63f22d36b074a4755f2ea8847393
CleoQc <cleoqc1124@gmail.com>
2020-06-24 16:26:50 -0400

    remove onclick on the logo

--- ui/src/views/Nav.tsx
+++ ui/src/views/Nav.tsx
@@ -66,7 +66,7 @@ export default class Nav extends Component<Props, {}> {
         return (
             <>
             <nav>
-                <a class='brand' onClick={() => this.props.modeQuestion()} data-tooltip='Change Mode'>
+                <a class='brand'>
                     {/* {this.props.platformImg && <img src={this.props.platformImg} class='Nav__platformImg' height={50}/>} */}
                     <img class='logo' src='https://i.ibb.co/2Zp0pyw/weblogo.png'/>
                 </a>
